44ec3f05ddbe6ce2163c1235a7bd8cc3c4689600
Th4tGuyyy <carson-dickinson@hotmail.com>
2020-02-09 01:20:04 -0600

    fixed glitch

--- Entity.cs
+++ Entity.cs
@@ -12,29 +12,6 @@ namespace rpgtest2020
 		//lvl = based off stats
 		//location
 
-		/*
-	 Job:
-		default stat mod
-		default inventory
-		special actions ex: shop, attacking, running
-		stack of interations to complete
-	 Race:
-		defulat stats
-		attack other races
-		only friendly interacts with same race
-
-	General:
-		order of day:
-			1. wake up
-			2. do job
-			3. go back to bed(spawn point) and sleep
-
-		Events what happen on day
-			- attack player or enemy if in range and "can" attack?
-			- run away if cant attack
-			- clear stack and go to bed
-
-		 */
 
 		protected String name;
 		protected int health;
